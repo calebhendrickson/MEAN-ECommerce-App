f44380062dbd6db21b7f73c2b14c69544ea201d0
unknown <chendric@iastate.edu>
2020-02-09 20:48:48 -0600

    configured mongolab db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: "mongodb://localhost:27017/bulldogdb",
+  // (local databasee) database: "mongodb://localhost:27017/bulldogdb",
+  database: "mongodb://tester:tester1@ds061681.mlab.com:61681/bulldogdb",
   secret: "mysecret"
 };
